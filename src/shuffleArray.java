ec197caedea5d6ec1aded9e689080e55eb4ece3a
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-04-13 15:12:28 -0700

    shuffle the array list

---
+++ src/shuffleArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class shuffleArray {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+
+        Random rand = new Random();
+
+        for(int i = 0; i < array.length; i++) {
+            // This generates random index->   rand.nextInt(array.length)
+            int randomIndexToSwap = rand.nextInt(array.length);
+            int temp = array[randomIndexToSwap];
+            array[randomIndexToSwap] = array[i];
+            array[i] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
