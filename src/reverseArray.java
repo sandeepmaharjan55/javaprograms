3a7f1f37f1653244b16474308a244c71089a9888
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-04-15 23:30:13 -0700

    rev array

---
+++ src/reverseArray.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class reverseArray {
+    public static void main(String[] args) {
+        int[] arrayInput={1,2,3,3,4,5,5};
+        //we need to reverse it to {5,4,3,2,1}
+        reverseArrayFunction(arrayInput);
+    }
+    public static void reverseArrayFunction(int[] hell){
+        int hellLength=hell.length;
+        int start =0;
+        int end = hellLength-1;
+        ArrayList <Integer> newArr=new ArrayList<>();
+        for (int i = end; i >=0 ; i--) {
+            newArr.add(hell[i]);
+        }
+        System.out.println(newArr);
+
+
+    }
+}
