280ddc6ab5bb83816b9d252f604353f0d296b6db
Sandeep Maharjan <sandeepmaharjan55@gmail.com>
2024-04-22 14:34:35 -0700

    array sorting

--- src/arraySorting.java
+++ src/arraySorting.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class arraySorting {
     public static void main(String[] args) {
+        Integer[] arrayList={10,2,4,5,3,9,4};
+        System.out.println(Arrays.asList(SortArray(arrayList)));
+    }
 
+    private static Integer[] SortArray(Integer[] arrayData) {
+
+//        for (int her:arrayData
+//             ) {
+//            System.out.println(her);
+//
+//        }
+
+        for (int i = 0; i < arrayData.length; i++) {
+            for (int j = i+1; j < arrayData.length; j++) {
+                if (arrayData[i] > arrayData[j]){
+                    int temp= arrayData[i];
+                    arrayData[i]=arrayData[j];
+                    arrayData[j]=temp;
+                }
+            }
+        }
+        return arrayData;
     }
 }
--- src/tryCatch.java
+++ src/tryCatch.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class tryCatch {
     public static void main(String[] args) {
-        try{
-            int[] inter={1,2,3};
-            System.out.println(inter[10]);
-        }
-        catch(Exception e){
-            System.out.println("Invalid index");
-        }
+//        try{
+//            int[] inter={1,2,3};
+//            System.out.println(inter[10]);
+//        }
+//        catch(Exception e){
+//            System.out.println("Invalid index");
+//        }
 //        finally {
 //            System.out.println("The 'try catch' is finished.");
 //        }
+
+        try{
+            Scanner getInput = new Scanner(System.in);
+            String newS = getInput.nextLine();
+            int nerrr= Integer.parseInt(newS);
+            System.out.println(nerrr);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
     }
 }
